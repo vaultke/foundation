d614905dafae004080af2e92d0fe517161015a3b
itscracker <douglasdaggs@gmail.com>
2022-01-09 08:58:15 +0300

    Fixed cryptKey Error

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -6,6 +6,8 @@ use yii\behaviors\AttributeBehavior;
 use vaultke\foundation\behaviors\Delete;
 class ActiveRecord extends \yii\db\ActiveRecord
 {
+	public $cryptKey = '';
+	
 	public function behaviors()
 	{
 	    return [
@@ -15,7 +17,7 @@ class ActiveRecord extends \yii\db\ActiveRecord
 			[
 				'class' => AttributeBehavior::class,
 				'attributes' => [
-					ActiveRecord::EVENT_BEFORE_INSERT => static::cryptKey().'_crypt_id',
+					ActiveRecord::EVENT_BEFORE_INSERT => $this->cryptKey.'_crypt_id',
 				],
 				'value' => function ($event) {
 					return hash_hmac('sha256', md5(date('DYM').substr(str_shuffle("0123456789abcdefghijklmnopqrstvwxyz"), 0, 10)), sha1(date('myd').rand(0,9999999999)));
@@ -23,5 +25,6 @@ class ActiveRecord extends \yii\db\ActiveRecord
 			],
 	    ];
 	}
+
 
 }
